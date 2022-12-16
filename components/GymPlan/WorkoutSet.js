88293b203eb872fd604c98156c1f84a7811ac265
Kirsten Schuhmann <kirsten.schuhmann@hotmail.de>
2022-12-16 13:02:21 +0100

    updated input fields to adjust the screen size

--- components/GymPlan/WorkoutSet.js
+++ components/GymPlan/WorkoutSet.js
@@ -2,44 +2,46 @@ import styled from "styled-components";
 
 export default function WorkoutSet({ setName }) {
   return (
-    <>
-      <StyledSection>
-        <h3> {setName} </h3>
+    <StyledSection>
+      <h3> {setName} </h3>
 
-        <StyledDiv>
-          <input
-            id="number"
-            type="number"
-            name="set"
-            label="set"
-            placeholder="set"
-          />
-          <p> x </p>
-          <input
-            id="volume"
-            type="number"
-            name="volume"
-            label="volume"
-            placeholder="vol"
-          />
-          <input
-            id="exercise"
-            type="text"
-            name="exercise"
-            label="exercise"
-            autoComplete="off"
-            placeholder="exercise"
-          />
-          <input
-            id="weight"
-            type="number"
-            name="weight"
-            label="weight"
-            placeholder="weight"
-          />
-        </StyledDiv>
-      </StyledSection>
-    </>
+      <StyledDiv>
+        <input
+          aria-label="set number"
+          type="number"
+          name="set"
+          label="set"
+          placeholder="set"
+          min="0"
+        />
+        <p> x </p>
+        <input
+          aria-label="set volume"
+          type="number"
+          name="volume"
+          label="volume"
+          placeholder="vol"
+          min="0"
+        />
+
+        <input
+          aria-label="set weight"
+          type="number"
+          name="weight"
+          label="weight"
+          placeholder="weight"
+          min="0"
+        />
+        <input
+          aria-label="set exercise"
+          type="text"
+          name="exercise"
+          label="exercise"
+          autoComplete="off"
+          placeholder="exercise"
+        />
+      </StyledDiv>
+    </StyledSection>
   );
 }
 
@@ -48,8 +50,8 @@ const StyledSection = styled.section`
   border-radius: 15px;
   padding: 13px;
   margin: 0 1rem 1.5rem 1rem;
-  overflow: hidden;
-  min-width: 265px;
+  min-width: 300px;
+  max-width: 700px;
 
   h3 {
     margin: 2px;
@@ -59,18 +61,18 @@ const StyledSection = styled.section`
 
 const StyledDiv = styled.div`
   display: flex;
-  justify-content: center;
+  flex-flow: row wrap;
   align-items: center;
 
   p {
     font-size: small;
-    grid-column: 2;
   }
 
   input {
     height: 30px;
     border-radius: 5px;
     border: 1.5px solid lightgray;
+
     margin: 8px;
     text-align: center;
   }
@@ -85,31 +87,31 @@ const StyledDiv = styled.div`
     // Firefox - hide arrows (CSS - letters shouldn't be allowed! Needs to be fixed later!!!)
     -moz-appearance: textfield;
     // normal styling
-    background-color: lightgrey;
-    border: 0.5px solid transparent;
+    background-color: #efefef;
+    border: 0.5px solid lightgrey;
+    font-weight: 600;
 
     ::placeholder {
       color: grey;
+      font-weight: 500;
       font-size: 15px;
     }
   }
-
-  input[name="set"] {
-    grid-column: 1;
-  }
-
   input[name="set"],
   input[name="volume"] {
-    min-width: 40px;
-    max-width: 65px;
-  }
-
-  input[name="exercise"] {
-    min-width: 79px;
-    width: 90%;
+    min-width: 35px;
+    flex-basis: 0;
+    flex-grow: 0.5;
   }
 
   input[name="weight"] {
     min-width: 55px;
+    flex-basis: 0;
+    flex-grow: 1;
+  }
+  input[name="exercise"] {
+    min-width: 70px;
+    flex-basis: 0;
+    flex-grow: 3;
   }
 `;
