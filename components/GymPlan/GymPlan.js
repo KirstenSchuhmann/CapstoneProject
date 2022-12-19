7abca2daca1c64b71ed13b403994622ca32e8db2
Kirsten Schuhmann <115224131+KirstenSchuhmann@users.noreply.github.com>
2022-12-19 11:02:10 +0100

    added suggestion -> form tag from merle
    Co-authored-by: merlemak <merlemakoben@gmail.com>

--- components/GymPlan/GymPlan.js
+++ components/GymPlan/GymPlan.js
@@ -2,12 +2,12 @@ import WorkoutSet from "./WorkoutSet";
 
 export default function GymPlan() {
   return (
-    <>
+    <form>
       <WorkoutSet setName="Warm-up" />
       <WorkoutSet setName="Squat" />
       <WorkoutSet setName="Bench" />
       <WorkoutSet setName="Deadlift" />
       <WorkoutSet setName="Assistant" />
-    </>
+    </form>
   );
 }
