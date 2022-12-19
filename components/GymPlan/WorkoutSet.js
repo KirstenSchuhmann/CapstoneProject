fe09856ce4f8cba9fd4133122424ce9e7366cf5c
Kirsten Schuhmann <kirsten.schuhmann@hotmail.de>
2022-12-19 11:40:48 +0100

    outsourced StyledDiv, which includes the input fields to file ExerciseSet.js

--- components/GymPlan/WorkoutSet.js
+++ components/GymPlan/WorkoutSet.js
@@ -1,46 +1,11 @@
 import styled from "styled-components";
+import ExerciseSet from "./ExerciseSet";
 
 export default function WorkoutSet({ setName }) {
   return (
     <StyledSection>
       <h3> {setName} </h3>
-
-      <StyledDiv>
-        <input
-          aria-label="set number"
-          type="number"
-          name="set"
-          label="set"
-          placeholder="set"
-          min="0"
-        />
-        <p> x </p>
-        <input
-          aria-label="set volume"
-          type="number"
-          name="volume"
-          label="volume"
-          placeholder="vol"
-          min="0"
-        />
-
-        <input
-          aria-label="set weight"
-          type="number"
-          name="weight"
-          label="weight"
-          placeholder="weight"
-          min="0"
-        />
-        <input
-          aria-label="set exercise"
-          type="text"
-          name="exercise"
-          label="exercise"
-          autoComplete="off"
-          placeholder="exercise"
-        />
-      </StyledDiv>
+      <ExerciseSet />
     </StyledSection>
   );
 }
@@ -56,62 +21,5 @@ const StyledSection = styled.section`
   h3 {
     margin: 2px;
     padding-left: 5px;
-  }
-`;
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-
-  p {
-    font-size: small;
-  }
-
-  input {
-    height: 30px;
-    border-radius: 5px;
-    border: 1.5px solid lightgray;
-
-    margin: 8px;
-    text-align: center;
-  }
-
-  input[type="number"] {
-    width: 10%;
-    //Chrome, Safari, Edge, Opera - hide arrows
-    ::-webkit-outer-spin-button,
-    ::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-    }
-    // Firefox - hide arrows (CSS - letters shouldn't be allowed! Needs to be fixed later!!!)
-    -moz-appearance: textfield;
-    // normal styling
-    background-color: #efefef;
-    border: 0.5px solid lightgrey;
-    font-weight: 600;
-
-    ::placeholder {
-      color: grey;
-      font-weight: 500;
-      font-size: 15px;
-    }
-  }
-  input[name="set"],
-  input[name="volume"] {
-    min-width: 35px;
-    flex-basis: 0;
-    flex-grow: 0.5;
-  }
-
-  input[name="weight"] {
-    min-width: 55px;
-    flex-basis: 0;
-    flex-grow: 1;
-  }
-  input[name="exercise"] {
-    min-width: 70px;
-    flex-basis: 0;
-    flex-grow: 3;
   }
 `;
